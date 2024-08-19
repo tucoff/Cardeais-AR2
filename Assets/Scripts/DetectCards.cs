6b58d5699c4af281137a30158ebf572038393a55
Artur Raffael <48564033+tucoff@users.noreply.github.com>
2024-08-19 16:27:52 -0300

    Checkpoint 4

--- Assets/Scripts/DetectCards.cs
+++ Assets/Scripts/DetectCards.cs
@@ -6,7 +6,11 @@ using TMPro;
 public class DetectCards : MonoBehaviour
 {
     public TextMeshProUGUI posText;
+    public TextMeshProUGUI posFullLife;
     string invocado = "Nenhum";
+
+    public int campo = 0; // 0 = N, 1 = L, 2 = S, 3 = O
+    public int pos = 0; // 0 = Atk, 1 = Def
 
     void Start()
     {
@@ -17,6 +21,34 @@ public class DetectCards : MonoBehaviour
     {
         invocado = other.gameObject.name;
         posText.text = other.gameObject.name;
+        posFullLife.text = other.gameObject.GetComponent<CardAttributes>().getLife().ToString();
+        switch (campo) 
+        {
+            case 0:
+                if(pos == 0)
+                {GameObject.FindWithTag("Simulation").GetComponent<SimulationBehaviour>().campoNAtkPos = invocado;}
+                else
+                {GameObject.FindWithTag("Simulation").GetComponent<SimulationBehaviour>().campoNDefPos = invocado;}
+                break;            
+            case 1:
+                if(pos == 0)
+                {GameObject.FindWithTag("Simulation").GetComponent<SimulationBehaviour>().campoSAtkPos = invocado;}
+                else
+                {GameObject.FindWithTag("Simulation").GetComponent<SimulationBehaviour>().campoSDefPos = invocado;}
+                break;            
+            case 2:
+                if(pos == 0)
+                {GameObject.FindWithTag("Simulation").GetComponent<SimulationBehaviour>().campoLAtkPos = invocado;}
+                else
+                {GameObject.FindWithTag("Simulation").GetComponent<SimulationBehaviour>().campoLDefPos = invocado;}
+                break;               
+            case 3:
+                if(pos == 0)
+                {GameObject.FindWithTag("Simulation").GetComponent<SimulationBehaviour>().campoOAtkPos = invocado;}
+                else
+                {GameObject.FindWithTag("Simulation").GetComponent<SimulationBehaviour>().campoODefPos = invocado;}
+                break;
+        }
     }
 
     void OnTriggerExit(Collider other)
@@ -25,6 +57,34 @@ public class DetectCards : MonoBehaviour
         {
             invocado = "Nenhum";
             posText.text = "Nenhum";
+            posFullLife.text = "0";
+            switch (campo) 
+            {
+                case 0:
+                    if(pos == 0)
+                    {GameObject.FindWithTag("Simulation").GetComponent<SimulationBehaviour>().campoNAtkPos = "";}
+                    else
+                    {GameObject.FindWithTag("Simulation").GetComponent<SimulationBehaviour>().campoNDefPos = "";}
+                    break;            
+                case 1:
+                    if(pos == 0)
+                    {GameObject.FindWithTag("Simulation").GetComponent<SimulationBehaviour>().campoSAtkPos = "";}
+                    else
+                    {GameObject.FindWithTag("Simulation").GetComponent<SimulationBehaviour>().campoSDefPos = "";}
+                    break;            
+                case 2:
+                    if(pos == 0)
+                    {GameObject.FindWithTag("Simulation").GetComponent<SimulationBehaviour>().campoLAtkPos = "";}
+                    else
+                    {GameObject.FindWithTag("Simulation").GetComponent<SimulationBehaviour>().campoLDefPos = "";}
+                    break;               
+                case 3:
+                    if(pos == 0)
+                    {GameObject.FindWithTag("Simulation").GetComponent<SimulationBehaviour>().campoOAtkPos = "";}
+                    else
+                    {GameObject.FindWithTag("Simulation").GetComponent<SimulationBehaviour>().campoODefPos = "";}
+                    break;
+            }
         }
     }
 }
--- Assets/Scripts/Summon.cs
+++ Assets/Scripts/Summon.cs
@@ -1,11 +1,29 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
 
 public class Summon : MonoBehaviour
 {
-    public void AskPosition()
+    CardAttributes cardAttributes;
+    public Image i;
+    public TextMeshProUGUI n;
+    public TextMeshProUGUI h;
+    public TextMeshProUGUI l;
+    public TextMeshProUGUI d;
+
+    void Start()
     {
-        Debug.Log("Where would you like to summon the card?");
+        cardAttributes = GetComponent<CardAttributes>();
+    }
+
+    public void PassAtt()
+    {
+        i.sprite = cardAttributes.getImage();
+        d.text = cardAttributes.getDamage().ToString();
+        n.text = cardAttributes.getCardName();
+        l.text = cardAttributes.getLife().ToString();
+        h.text = cardAttributes.getHabilidadeEspecial();
     }
 }
